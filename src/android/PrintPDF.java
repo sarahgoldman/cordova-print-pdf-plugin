b76e2b2b6d6ec2d6e82bbd2e1cb33823d8901f28
Sarah Goldman <sarahrgoldman@gmail.com>
2015-07-20 15:19:33 -0400

    setting up android platform

---
+++ src/android/PrintPDF.java
@@ -0,0 +1,6 @@
+package com.sgoldm.plugin;
+ 
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
